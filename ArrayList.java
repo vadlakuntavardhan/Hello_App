1c9adc322cff0b82d2505580aab8a9cec2a9d614
vadlakuntavrdhan <vadlakuntavardhan@gmail.com>
2026-03-25 00:38:33 +0530

    Added UC5: Display Hello with multiple command line arguments

--- ArrayList.java
+++ ArrayList.java
@@ -1,10 +1,5 @@
-class ArrayList{
-   public static void main(String[] args ){
-   
-   // This is "Hello App Program"
-   
-    String msg = "This is Dev Branch";
-	system.out.println(msg);
-	}
-}	
-	
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
